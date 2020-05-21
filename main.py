299f2101a01e103d76b4b25cb347bb4861117478
nwgat <github@nwgat.net>
2020-05-21 20:15:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,9 @@
 
 import machine
 import urequests
-import time
 
 # MailNinja Notification
+response = urequests.get('http://sub.yourdomain.com:9292/hooks/mailninja?token=42')
 
-response = urequests.get('http://hook.yourdomain.com/hooks/mailninja?token=42')
-response = urequests.get('http://yourmachine:9000/hooks/mailninja?token=42') # if you run webhook on a local machine
-#response = urequests.get('http://192.168.4.2:9000/hooks/mailninja?token=42') # if your connected to esp8266 via its AP
-
-#  sleep, keep it higher when you test and to 2 when your ready
-sleep(2)
-
-# put the device to sleep (uncomment when your done testing)
+# Power Saving mode (RST + GND to wake up with a reed door sensor)
 #machine.deepsleep()
