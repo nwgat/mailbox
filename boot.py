96a5181c3f2d3cdde0a0523331a58dd71f62c914
nwgat <github@nwgat.net>
2020-05-21 19:05:22 +0200

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,8 @@
+import network
+import main
+
+# wifi
+sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+sta_if.scan()
+sta_if.connect("your_ssid", "yourwifipassword")
+sta_if.isconnected()
