b68748d9af665e73343ce4c94116166b0dc667f2
nwgat <github@nwgat.net>
2020-05-22 05:11:09 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -7,3 +7,4 @@ import main
 # connect wifi
 sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
 sta_if.connect("your_ssid", "yourwifipassword")
+#sta_if.ifconfig(config=('192.168.1.4', '255.255.255.0', '192.168.1.1', '1.1.1.1')) # static ip
