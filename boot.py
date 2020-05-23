17f558bc0bf623aa15a09af0502f328de084ce75
nwgat <github@nwgat.net>
2020-05-23 04:18:42 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,10 +1,10 @@
 # boot.py
 # nwgat.ninja
 # https://nwgat.ninja/mailboxninja
+
 import network
 import main
 
 # connect wifi
-sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+sta_if = network.WLAN(network.STA_IF)
 sta_if.connect("your_ssid", "yourwifipassword")
-#sta_if.ifconfig(config=('192.168.1.4', '255.255.255.0', '192.168.1.1', '1.1.1.1')) # static ip
