4cce70672a6d05da9753cb27b20eedf3a1d87c5f
nwgat <github@nwgat.net>
2020-05-23 04:19:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import urequests
 import time
 
 # MailNinja Notification
-response = urequests.get('http://sub.yourdomain.com:9292/hooks/mailninja?token=42')
+response = urequests.get('http://yourdomain.com:9292/hooks/mailninja?token=42')
 
 # Power Saving mode (RST + GND to wake up with a reed door sensor)
 time.sleep(5)
