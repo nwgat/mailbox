3d2729d883cb52392dfac8768c0b085c78b81815
nwgat <github@nwgat.net>
2020-05-26 09:48:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 # nwgat.ninja
 # https://nwgat.ninja/mailboxninja
 
-import urequests
-import machine
+import urequests, machine
 
 # MailNinja Notification
 hook = urequests.get('http://yourserver:9292/hooks/mailninja?token=42')
@@ -11,7 +10,7 @@ hook = urequests.get('http://yourserver:9292/hooks/mailninja?token=42')
 # show network status
 status = urequests.get("http://yourserver.com:9292")
 print ('--------------------')
-print ('Network Status:', (status.content))
+print ('Internet Status:', (status.text))
 print ('--------------------')
 
 # Power Saving mode (RST + GND to wake up with a reed door sensor)
