899f80071177b662e2074bc87306bff868b10dbd
nwgat <github@nwgat.net>
2020-05-27 04:59:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # nwgat.ninja
 # config.py
-ssid = 'yournetwork' 
+ssid = 'yournetwork24' 
 ssid_pw = 'yourpassword'
 webhook = 'http://yourserver:9292/hooks/mailninja?token=42'
 webhook_server = 'http://yourserver:9292'
