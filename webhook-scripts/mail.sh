488cd8d488b1c4de4fd2a0246b9d66ba2c2da2fc
nwgat <github@nwgat.net>
2020-08-12 05:45:04 +0200

    Update mail.sh

--- webhook-scripts/mail.sh
+++ webhook-scripts/mail.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # download https://github.com/muquit/mailsend-go/releases/download/v1.0.9/mailsend-go_1.0.9_linux-64bit.tar.gz
-mailsend-go -sub "Test"  -smtp smtp.gmail.com -port 587 \
+# usage: mail.sh your@email.com
+./mailsend-go -sub "MailboxNinja"  -smtp server.com -port 587 \
      auth \
-      -user jsnow@gmail.com -pass "secret" \
-     -from "jsnow@gmail.com" -to  "mjane@example.com" \
+      -user hello@nwgat.ninja -pass "yourpassword" \
+     -from "hello@nwgat.ninja" -to  ""$1"" \
      body \
        -msg "i pity the fool that dont pick up his mail ;)"
